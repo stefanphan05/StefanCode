a68cb1c9f0b3a4d2e3e8157613e25f18d93a5dee
Stefan Phan <164993942+stefanphan05@users.noreply.github.com>
2026-09-21 21:40:54 +0930

    Time: 0 ms (100.00%) | Memory: 19.3 MB (57.08%) - LeetSync

--- 2507-number-of-common-factors/number-of-common-factors.py
+++ 2507-number-of-common-factors/number-of-common-factors.py
@@ -1,19 +1,9 @@
 class Solution:
     def commonFactors(self, a: int, b: int) -> int:
-        la = set()
         count = 0
-        if b > a:
-            a, b = b, a
-        elif a == b:
-            count += 1
 
-        for i in range(1, a // 2 + 1):
-            if a % i == 0:
-                la.add(i)
-        
-        for i in range(1, b + 1):
-            if b % i == 0:
-                if i in la:
-                    count += 1
+        for i in range(1, min(a, b) + 1):
+            if a % i == 0 and b % i == 0:
+                count += 1
 
         return count
